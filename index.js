2d7c93952e84b07ca760fe4c7a4a3e4c307b76e2
Mohammad Atallah <m.atallah@beno.com>
2021-04-26 21:04:55 +0400

    Implement /now

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT
 
+app.get('/now', (req, res) => {
+  res.status(200).send(new Date().toLocaleString())
+})
+
 app.get('/', (req, res) => {
   res.send('Welcome to time-machine!')
 })
