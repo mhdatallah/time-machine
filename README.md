bab70be0599bf33c4f37eabe4b9ce51e09aa3a60
Mohammad Atallah <m.atallah@beno.com>
2021-04-26 21:11:34 +0400

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # time-machine
+
+A Node.js server for timezone conversion.
+
+
+
+## Prerequisites
+
+- Node.js v14+
+
+
+
+## How to run
+
+1. Install the dependencies:
+
+```bash
+npm install
+```
+
+2. Create `.env` file in the project root
+3. Set up the following environment variables in `.env`:
+
+```bash
+NODE_ENV=local
+PORT=4000
+```
+
+4. Execute [index.js](./index.js):
+
+```bash
+node index.js
+```
+
--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 require('dotenv').config();
 const express = require('express')
-const app = express()
-const port = process.env.PORT
+const app = express();
+const port = process.env.PORT;
 
 app.get('/now', (req, res) => {
-  res.status(200).send(new Date().toLocaleString())
+  msg = new Date().toUTCString()
+  res.status(200).send(msg);
 })
 
 app.get('/', (req, res) => {
-  res.send('Welcome to time-machine!')
+  res.send('Welcome to time-machine!');
 })
 
 app.listen(port, () => {
   if (process.env.NODE_ENV !== 'production') {
-    console.log(`time-machine listening at http://localhost:${port}`)
+    console.log(`time-machine listening at http://localhost:${port}`);
   }
 })
